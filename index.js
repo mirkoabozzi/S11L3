ecd50d4b94ff99f357c3b755577fefea05b05275
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-10 16:32:07 +0200

    prove ts

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("Primo file TS");
+let myNumber = 10;
+const myString = "mirko";
+console.log(myString.toUpperCase());
+const array = [];
+array.push(5, "mirko");
+console.log(array);
+const persona = {
+    nome: "mirko",
+    cognome: "abozzi",
+};
+persona.eta;
+let prova = false;
---
+++ index.ts
@@ -0,0 +1,21 @@
+console.log("Primo file TS");
+
+let myNumber = 10;
+const myString: string = "mirko";
+
+console.log(myString.toUpperCase());
+
+const array: number[] = [];
+
+array.push(5, "mirko");
+
+console.log(array);
+
+const persona = {
+  nome: "mirko",
+  cognome: "abozzi",
+};
+
+persona.eta;
+
+let prova: string | boolean = false;
